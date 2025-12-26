685d03d7571f7d667958584ba272a39d55d1f77e
bala-amypo <bala@amypo.in>
2025-12-26 08:54:44 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,37 +1,20 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
-public class SwaggerConfig {
+public class OpenApiConfig {
 
     @Bean
-    public OpenAPI openAPI() {
-
-        SecurityScheme securityScheme = new SecurityScheme()
-                .name("Authorization")
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT");
-
-        Server server = new Server();
-        server.setUrl("http://localhost:9087");
-        server.setDescription("Local Server");
-
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .addServersItem(server)
-                .info(new Info()
-                        .title("Multi-Location Inventory Balancer API")
-                        .version("1.0"))
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(new Components()
-                        .addSecuritySchemes("bearerAuth", securityScheme));
-    }
+                // You need to change the port as per your server
+                .servers(List.of(
+                        new Server().url("https://9233.408procr.amypo.ai/")
+                ));
+        }
 }
